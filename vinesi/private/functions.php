9db34ecac53f929545e513dd85df8bd8b21fb5a1
nedzof <nedzofetahovic@hotmail.com>
2018-11-21 16:22:32 +0100

    Commit

--- vinesi/private/functions.php
+++ vinesi/private/functions.php
@@ -6,11 +6,18 @@
  * Time: 13:35
  */
 
+include ('initialize.php');
+
 function url_for ($script_path) {
     //adding leading / if not here
     if ($script_path[0] != '/'){
         $script_path = "/" . $script_path;
     }
 
-    return WWW_ROOT . $script_path;
+    $_wwwRoot = WWW_ROOT . $script_path;
+    return $_wwwRoot;
 }
+
+echo WWW_ROOT;
+echo 'Here is from function.php';
+echo url_for('files/home.php');
--- vinesi/public/shared/header.php
+++ vinesi/public/shared/header.php
@@ -1,10 +1,14 @@
+<?php
+   if(!isset($page_title)) {
+       $page_title = 'Admin Area'; }
+ ?>
+
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Vinesi</title>
+    <title>Vinesi - <?php echo $page_title; ?></title>
     <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
@@ -25,9 +29,10 @@
 <body>
 <div>
     <nav class="navbar navbar-light navbar-expand-md navigation-clean">
-        <div class="container"><a class="navbar-brand" href="<?php echo url_for('/files/home.php)')?>"
+        <div class="container"><a class="navbar-brand" href="<?php echo url_for('files/home.php');?>"
                                   style="color: #17a2b8;font-size: 32px;font-weight: 300;"><i class="fa fa-ravelry"></i>Vinesi</a>
-            <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span>
+                <span
                         class="navbar-toggler-icon"></span></button>
             <div
                     class="collapse navbar-collapse" id="navcol-1">
