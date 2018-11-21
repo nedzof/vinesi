b598dccbfe02e95c507f10b739f66b763ea69b5b
nedzo <nedzofetahovic@hotmail.com>
2018-11-21 10:42:17 +0100

    xampp

---
+++ Vinesi/private/initialize.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nedzo
+ * Date: 21.11.18
+ * Time: 10:18
+ */
--- Vinesi/public/shared/footer.php
+++ Vinesi/public/shared/footer.php
@@ -1,3 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Vinesi</title>
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="../assets/css/-Login-form-Page-BS4-.css">
+    <link rel="stylesheet" href="../assets/css/Data-Summary-Panel---3-Column-Overview--Mobile-Responsive.css">
+    <link rel="stylesheet" href="../assets/css/Data-Table-1.css">
+    <link rel="stylesheet" href="../assets/css/Data-Table.css">
+    <link rel="stylesheet" href="../assets/css/Footer-Clean.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="../assets/css/Projects-Horizontal.css">
+    <link rel="stylesheet" href="../assets/css/register.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/Team-Clean.css">
+</head>
 <div class="footer-clean">
     <footer>
         <div class="container">
@@ -20,7 +40,6 @@
                 </div>
                 <div class="col-lg-3 item social"><a href="#"><i class="fa fa-ravelry"></i></a>
                     <p class="copyright">Vinesi © 2018</p>
-                 <!--   &copy; <?php echo date('Y'); ?> -->
                 </div>
             </div>
         </div>
