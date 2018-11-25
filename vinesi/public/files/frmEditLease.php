864c0c320fab698cfe8ecf73074c784e18312e6e
nedzof <nedzofetahovic@hotmail.com>
2018-11-25 20:06:59 +0100

    Commit

--- vinesi/public/files/frmEditLease.php
+++ vinesi/public/files/frmEditLease.php
@@ -31,15 +31,16 @@
             <div class="intro">
                 <h2 class="text-center" style="font-weight: normal;"><strong>Edit</strong>&nbsp;Tenancy</h2>
                 <form>
-                    <div class="form-grou§p"><label class="text-secondary">Unit</label><input class="form-control" type="text" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$" inputmode="email"></div>
-                    <div class="form-group"><label class="text-secondary">Tenant Name</label><input class="form-control" type="password" required=""></div>
-                    <div class="form-group"><label class="text-secondary">Adress</label><input class="form-control" type="password" required=""></div>
-                    <div class="form-group"><label class="text-secondary">Area (in square meter)</label><input class="form-control" type="password" required=""></div>
-                    <div class="form-group"><label class="text-secondary">Lease Term</label><input class="form-control" type="password" required=""></div>
+                    <div class="form-grou§p"><label class="text-secondary">Monthly Rent</label><input class="form-control" type="text" required="" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$" inputmode="email"></div>
+                    <div class="form-group"><label class="text-secondary">Utilities</label><input class="form-control" type="password" required=""></div>
+                    <div class="form-group"><label class="text-secondary">Payment Method</label><input class="form-control" type="password" required=""></div>
+                    <div class="form-group"><label class="text-secondary">Deposit</label><input class="form-control" type="password" required=""></div><button class="btn btn-info mt-2" type="submit" style="max-height: -8px;"><i class="icon ion-ios-compose-outline"></i></button></form>
                     <div class="form-group"><label class="text-secondary">Lease Start</label><input class="form-control" type="password" required=""></div>
                     <div class="form-group"><label class="text-secondary">Lease Expiry</label><input class="form-control" type="password" required=""></div>
-                    <div class="form-group"><label class="text-secondary">Rent</label><input class="form-control" type="password" required=""></div>
-                    <div class="form-group"><label class="text-secondary">Notes</label><input class="form-control" type="password" required=""></div><button class="btn btn-info mt-2" type="submit" style="max-height: -8px;"><i class="icon ion-ios-compose-outline"></i></button></form>
+                <div class="form-group"><label class="text-secondary">Property</label><input class="form-control" type="password" required=""></div>
+                <div class="form-group"><label class="text-secondary">Tenant</label><input class="form-control" type="password" required=""></div>
+
+
             </div>
         </div>
     </div>
