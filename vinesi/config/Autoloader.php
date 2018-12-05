b587d152d98a1b992c373e581262a6d857473e0e
Victoria Villar <villar.120045@e.unavarra.es>
2018-12-05 11:00:25 +0100

    config file

---
+++ vinesi/config/Autoloader.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Victoria Villar
+ * Date: 05/12/2018
+ * Time: 10:27
+ */
+
+namespace config;
+
+class Autoloader
+{
+    public static function autoload($str) {
+        $newPath = str_replace('\\', '/', $str);
+        $fileName = $newPath . '.php';
+
+        if (file_exists($fileName)) {
+            include_once($fileName);
+        } else {
+            return false;
+        }
+    }
+}
+
+spl_autoload_register('config\Autoloader::autoload');
