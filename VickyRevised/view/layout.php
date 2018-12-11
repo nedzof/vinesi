53b5b5398ee2769e9b6ffa1b26d495f9d5618fae
nedzof <nedzofetahovic@hotmail.com>
2018-12-11 18:14:27 +0100

    Commit

--- VickyRevised/view/layout.php
+++ VickyRevised/view/layout.php
@@ -1,12 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Victoria Villar
- * Date: 19/11/2018
- * Time: 16:07
- */
-function layoutAddContent($content) {
-    require_once("header.php");
-    require_once ($content);
-    require_once("footer.php");
-}
+
+echo $this->header;
+echo $this->content;
+echo $this->footer;
