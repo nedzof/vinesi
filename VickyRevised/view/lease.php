66b3ee0241a3466949e7ecb0062ce67a2f38ec86
nedzof <nedzofetahovic@hotmail.com>
2018-12-16 16:01:30 +0100

    Commit

--- VickyRevised/view/lease.php
+++ VickyRevised/view/lease.php
@@ -72,8 +72,8 @@ use view\TemplateView;
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                                     <button type="button" data-method="delete"
-                                            href="lease/delete/<?php echo $lease->getLeaseid(); ?>"
-                                            class="btn btn-primary">Delete
+                                            href="lease/delete/<?php echo $lease->getLeaseid(); ?>
+                                            class=" btn btn-primary">Delete
                                     </button>
                                 </div>
                             </div>
