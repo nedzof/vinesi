4bfb3f8b86c9c39af750d49c5b8c263155d1c053
nedzof <nedzofetahovic@hotmail.com>
2018-12-20 21:00:50 +0100

    Commit

--- VickyRevised/controller/EmailController.php
+++ VickyRevised/controller/EmailController.php
@@ -15,20 +15,20 @@ class EmailController
     {
         $emailView = new TemplateView("lease_PDF.php");
         $emailView->leasing = (new LeaseDAO())->getAllLeases();
-        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current customers", $emailView->render());
+        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current leases", $emailView->render());
     }
 
     public static function sendMeMyExpenses()
     {
         $emailView = new TemplateView("expense_PDF.php");
         $emailView->spending = (new ExpenseDAO())->getAllExpenses();
-        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current customers", $emailView->render());
+        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current expenses", $emailView->render());
     }
 
     public static function sendMeMyInvoices()
     {
         $emailView = new TemplateView("invoice_PDF.php");
         $emailView->invoicing = (new InvoiceDAO())->getAllInvoice();
-        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current customers", $emailView->render());
+        return EmailServiceClient::sendEmail(AuthServiceImpl::getInstance()->readUser()->getUseremail(), "My current invoices", $emailView->render());
     }
 }
