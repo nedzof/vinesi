2e97a53891fe8aa2a3179dea22c9d61ff2819936
nedzof <nedzofetahovic@hotmail.com>
2018-12-21 19:59:01 +0100

    Commit

--- database/Database.php
+++ database/Database.php
@@ -18,31 +18,38 @@ class Database
 
     protected function __construct()
     {
-        $_host = Config::get("database.host");
-        $_dbname = Config::get("database.name");
-        $_user = Config::get("database.user");
-        $_port = Config::get("database.port");
-        $_password = Config::get("database.password");
 
-        $connection_string="dsn=pgsql:host=$_host;dbname=$_dbname;user=$_user;password=$_password";
-        parse_str($connection_string, $params);
 
-        //$dsn = "pgsql:host=$_host;port=$_port;dbname=$_dbname;user=$_user;password=$_password";
+            self::$pdoInstance = new PDO (Config::get("database.dsn"), Config::get("database.user"), Config::get("database.password"));
+            self::$pdoInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        try {
-            // create a PostgreSQL database connection
-            //self::$pdoInstance = new PDO($dsn, $_user, $_password);
-            self::$pdoInstance = new PDO($params['dsn'], @$params['username'], @$params['password']);
 
-            // display a message if connected to the PostgreSQL successfully
-            if (self::$pdoInstance) {
-                // echo "Connected to the <strong>$_dbname</strong> database successfully!";
-                self::$pdoInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            echo $e->getLine();
-        }
+
+//        $_host = Config::get("database.host");
+//        $_dbname = Config::get("database.name");
+//        $_user = Config::get("database.user");
+//        $_port = Config::get("database.port");
+//        $_password = Config::get("database.password");
+//
+//        $connection_string="dsn=pgsql:host=$_host;dbname=$_dbname;user=$_user;password=$_password";
+//        parse_str($connection_string, $params);
+//
+//        //$dsn = "pgsql:host=$_host;port=$_port;dbname=$_dbname;user=$_user;password=$_password";
+//
+//        try {
+//            // create a PostgreSQL database connection
+//            //self::$pdoInstance = new PDO($dsn);
+//            self::$pdoInstance = new PDO($params['dsn'], @$params['username'], @$params['password']);
+//
+//            // display a message if connected to the PostgreSQL successfully
+//            if (self::$pdoInstance) {
+//                // echo "Connected to the <strong>$_dbname</strong> database successfully!";
+//                self::$pdoInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//            }
+//        } catch (PDOException $e) {
+//            echo $e->getMessage();
+//            echo $e->getLine();
+//        }
 
 
         /* self::$pdoInstance = new PDO($dsn, $user, )
